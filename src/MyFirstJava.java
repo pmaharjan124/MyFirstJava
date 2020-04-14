4535a250b34c6f99fc200fc97d730672beb45313
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-14 19:51:02 -0400

    Second commit

--- src/MyFirstJava.java
+++ src/MyFirstJava.java
@@ -5,7 +5,7 @@ This is the first Java program which will print ou
 public class MyFirstJava {
     public static void main (String[] args)
     {
-        System.out.println("Hello Puja. You are great!");
+        System.out.println("Hello Kathmandu. You are great!");
 
 
     }
